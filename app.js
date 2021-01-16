a8b3da3bd534edf4c829064b489b3c6c25d7baf7
PaulTervoort <PaulTervoortMail@gMail.com>
2021-01-16 14:50:54 +0100

    Game UI changed a bit

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const websocket = require("ws");
 var port = process.argv[2];
 var app = express();
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res) 
+{
   res.sendFile("splash.html", { root: "./public" });
 });
-app.get("/play", function(req, res) {
+app.get("/play", function(req, res) 
+{
   res.sendFile("game.html", { root: "./public" });
 });
 
@@ -18,21 +20,28 @@ const server = http.createServer(app);
 
 const wss = new websocket.Server({ server });
 
-wss.on("connection", function (ws) {
-    /*
-     * let's slow down the server response time a bit to 
-     * make the change visible on the client side
-     */ 
-    setTimeout(function () {
-        console.log("Connection state: " + ws.readyState);
-        ws.send("Thanks for the message. --Your server.");
-        ws.close();
-        console.log("Connection state: " + ws.readyState);
-    }, 2000);
+wss.on("connection", function (ws) 
+{
+  console.log("Connection state: " + ws.readyState);
+  ws.send("StartGame");
+  ws.close();
+  console.log("Connection state: " + ws.readyState);
 
-    ws.on("message", function incoming(message) {
-        console.log("[LOG] " + message);
-    });
+  ws.on("message", function incoming(message) {
+      console.log("[LOG] " + message);
+  });
 });
 
 server.listen(port);
+
+
+var games = [];
+var finishedGames = 0;
+
+var game = function(gameID) 
+{
+  this.id = gameID;
+  this.players = [null, null];
+  this.turn = 0;
+  if(Math.random() > 0.5) { turn = 1; }
+};
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,10 +1,17 @@
-const target = document.getElementById("hello");
+const popup = document.getElementById("PopUp");
+
+
 const socket = new WebSocket("ws://localhost:6969");
+
 socket.onmessage = function(event){
-    target.innerHTML = event.data;
+    if(event.data == "StartGame"){
+        popup.style.display = "none";
+    }
 };
 
 socket.onopen = function(){
     socket.send("Hello from the client!");
     target.innerHTML = "Sending a first message to the server ...";
 };
+
+
