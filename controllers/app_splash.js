1672baa01202fde0c295b5edd450fc5ac56c83b1
PaulTervoort <PaulTervoortMail@gMail.com>
2021-01-19 21:37:47 +0100

    -Changed splash background -Added 'rules' page -Used ejs-injection to sync rules text between rules and game pages

--- controllers/app_splash.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-        
-//     getStats: function(req, res) {console.log(app.startedGames);}
-    
-// }
-var app = require('../app.js');
-this.getStats=function(req, res) {console.log(app.);}
