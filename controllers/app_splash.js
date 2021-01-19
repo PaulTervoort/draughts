27015c89c6eec2b92b7b7e23b4fbf84d17f103f7
amy <amy+github@srab.nl>
2021-01-19 16:17:37 +0100

    Splash screen statistics

---
+++ controllers/app_splash.js
@@ -0,0 +1,7 @@
+// module.exports = {
+        
+//     getStats: function(req, res) {console.log(app.startedGames);}
+    
+// }
+var app = require('../app.js');
+this.getStats=function(req, res) {console.log(app.);}
